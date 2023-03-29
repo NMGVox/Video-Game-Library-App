091847f4167255017fa3655106734485a54a6c8f
NMGVox <nijeemgreene@gmail.com>
2023-03-29 16:56:05 -0400

    Add base styling for webpage

---
+++ script.js
@@ -0,0 +1,13 @@
+let my_collection = [];
+
+function VideoGame(title, developer, publisher, year_released, play_status) {
+    this.title = title;
+    this.developer = developer;
+    this.publisher = publisher;
+    this.year_released = year_released;
+    this.play_status = play_status;
+}
+
+function AddVideoGame() {
+    //Hm.
+}
